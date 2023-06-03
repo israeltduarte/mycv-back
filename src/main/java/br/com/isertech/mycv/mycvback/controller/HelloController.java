fadeea95b718d1141755dbfcfeb3a5062bd96bd6
Israel Duarte <israeltduarte@gmail.com>
2023-06-03 17:36:58 -0300

    add testing controller

--- src/main/java/br/com/isertech/mycv/mycvback/controller/HelloController.java
+++ src/main/java/br/com/isertech/mycv/mycvback/controller/HelloController.java
@@ -2,11 +2,15 @@ package br.com.isertech.mycv.mycvback.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/mycv")
 public class HelloController {
 
+    @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
         return ResponseEntity.status(HttpStatus.OK).body("Hello");
     }
